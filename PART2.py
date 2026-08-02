4a857545c3ce442d2f9f45c66d9b77fd5ef94a12
Fajar2511 <fajarkhokhar123@gmail.com>
2026-08-02 17:36:55 +0500

    Add files via upload

---
+++ PART2.py
@@ -0,0 +1,30 @@
+print("\nPART 2:Operators")
+
+print("Comparison Operators:")
+x = 5
+y = 10
+print("x == y:", x == y)
+print("x != y:", x != y)
+print("x > y:", x > y)
+print("x < y:", x < y)
+print("x >= y:", x >= y)
+print("x <= y:", x <= y)
+print("Logical Operators:")
+print("x and y:", x and y)
+print("x or y:", x or y)
+print("not x:", not x)
+print("not y:", not y)
+print("Assignment Operators:")
+z = 15
+print("z =", z)
+z += 5
+print("z += 5:", z)
+z -= 3
+print("z -= 3:", z)
+z *= 2
+print("z *= 2:", z)
+z /= 4
+print("z /= 4:", z)
+print("z %= 3:", z % 3)
+print("z **= 2:", z ** 2)
+print("z //= 2:", z // 2)  
