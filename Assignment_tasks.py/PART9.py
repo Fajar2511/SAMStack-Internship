4c1c1ba8983563949c7b5b2e98c70ca742112c70
Fajar2511 <fajarkhokhar123@gmail.com>
2026-08-02 17:42:52 +0500

    Delete Assignment_tasks.py directory

--- Assignment_tasks.py/PART9.py
+++
@@ -1,8 +0,0 @@
-print("PART 9: Modules and Packages")
-import math
-import random
-print("To demonstrate the use of math module:")
-print("Square root of 16:", math.sqrt(16))
-print("Factorial of 5:", math.factorial(5))
-print("To demonstrate the use of random module:")
-print("Random number between 1 and 10:", random.randint(1, 10))
